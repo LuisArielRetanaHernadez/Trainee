b71b202fc8a55dc40718904a2347166a06e06b7f
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-03-28 16:55:45 -0600

    agregando cat_random_fact

--- src/app.js
+++ src/app.js
@@ -1,9 +1,31 @@
+/* eslint-disable react/react-in-jsx-scope */
 
-export default function App() {
+import { useEffect, useState } from 'react'
+
+// dotenv
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+export default function App () {
+  const [fact, setFact] = useState(' ')
+
+  const CAT_RANDOM_FACT = 'https://catfact.ninja/fact'
+
+  useEffect(() => {
+    fetch(CAT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => setFact(data.fact))
+  }, [])
+
+  useEffect(() => {
+
+  }, [fact])
 
   return (
     <div>
       <h1>App</h1>
+     { fact && <p>{fact}</p> }
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import App from './app'
 const root = createRoot(document.getElementById('root'))
 
 root.render(
+  // eslint-disable-next-line react/react-in-jsx-scope
   <App />
 )
